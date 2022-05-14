6828b2db85502cfefdcb2bd3891a615c944b3673
无名氏 <chartjin15@gmail.com>
2022-05-15 01:31:27 +1000

    更新

--- files/manga_check.py
+++ files/manga_check.py
@@ -8,7 +8,8 @@ try:
             if line[2] == "1" and line[4] == line[5]:
                 read_it.append(line[0])
 
+        print("目前有记录的已完全阅读日本漫画作品：")
         for ch_name in read_it:
-            print(ch_name)
+            print("- " + ch_name)
 except FileNotFoundError:
     print("Missing file!")
