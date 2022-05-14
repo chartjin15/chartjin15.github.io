3a444b45081d68430aebddd4d9860dcf6a8ef5bb
无名氏 <chartjin15@gmail.com>
2022-05-15 06:22:22 +1000

    更新

--- files/manga_check.py
+++ files/manga_check.py
@@ -8,7 +8,17 @@ try:
             if line[2] == "1" and line[4] == line[5]:
                 read_it.append(line[0])
 
-        print("目前有记录的已完全阅读日本漫画作品：")
+        print("目前已完全阅读的日本漫画作品：")
+        for ch_name in read_it:
+            print("- " + ch_name)
+        
+        read_it = []
+        for line in lines:
+            line = str(line).split(",")
+            if line[4] != line[5]:
+                read_it.append(line[0])
+
+        print("目前尚未阅读完的日本漫画作品：")
         for ch_name in read_it:
             print("- " + ch_name)
 except FileNotFoundError:
