a12150f5fdc9710f2253ef71f16f58bbde1adc66
无名氏 <chartjin15@gmail.com>
2022-05-15 01:31:40 +1000

    更新

--- files/sort_helper.ps1
+++ files/sort_helper.ps1
@@ -1,4 +1,4 @@
 $filepath = "./manga_tracking.csv"
 Move-Item $filepath "temp.file"
-Get-Content -Encoding "utf8" -Path "temp.file" | Sort-Object | Out-File -FilePath $filepath
+Get-Content -Encoding "utf8" -Path "temp.file" | Sort-Object | Out-File -Encoding "utf8" -FilePath $filepath
 Remove-Item "temp.file"
