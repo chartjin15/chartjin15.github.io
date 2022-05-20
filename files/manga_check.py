642c3e5ed3ebcb7f81e74226a4d07136e6b17974
无名氏 <chartjin15@gmail.com>
2022-05-21 01:10:30 +1000

    更新

--- files/manga_check.py
+++ files/manga_check.py
@@ -2,24 +2,31 @@ try:
     with open("./files/manga_tracking.csv", encoding="UTF-8") as fileobj:
         lines = fileobj.read().splitlines()
 
-        read_it = []
-        for line in lines:
+        status = [[], [], [], []]
+        for line in lines[1:]:
             line = str(line).split(",")
-            if line[2] == "1" and line[4] == line[5]:
-                read_it.append(line[0])
+            if line[2] == "2":
+                status[3].append(line[0] + "\t最终读至第" + line[5] + "话")
+            elif line[2] == "0":
+                if line[4] != line[5] and line[5] != "0":
+                    status[0].append(line[0] + "\t仍差" + str(int(line[4]) - int(line[5])) + "话")
+            else:
+                if line[4] == line[5]:
+                    status[2].append(line[0] + "\t已读完")
+                else:
+                    status[1].append(line[0] + "\t仍差" + str(int(line[4]) - int(line[5])) + "话")
 
-        print("目前已完全阅读的日本漫画作品：")
-        for ch_name in read_it:
+        print("目前未读完的连载日本漫画作品：")
+        for ch_name in status[0]:
             print("- " + ch_name)
-        
-        read_it = []
-        for line in lines:
-            line = str(line).split(",")
-            if line[4] != line[5]:
-                read_it.append(line[0])
-
-        print("目前尚未阅读完的日本漫画作品：")
-        for ch_name in read_it:
+        print("\n目前未读完的完结日本漫画作品：")
+        for ch_name in status[1]:
+            print("- " + ch_name)
+        print("\n目前已读完的完结日本漫画作品：")
+        for ch_name in status[2]:
+            print("- " + ch_name)
+        print("\n目前已弃坑的难看日本漫画作品：")
+        for ch_name in status[3]:
             print("- " + ch_name)
 except FileNotFoundError:
     print("Missing file!")
--- files/manga_sort.ps1
+++ files/manga_sort.ps1
@@ -1,4 +1,4 @@
 $filepath = "./manga_tracking.csv"
-Move-Item $filepath "temp.file"
-Get-Content -Encoding "utf8" -Path "temp.file" | Sort-Object | Out-File -Encoding "utf8" -FilePath $filepath
-Remove-Item "temp.file"
+$temp = Get-Content -Encoding "utf8" -Path $filepath
+$temp[0] | Set-Content -Encoding "utf8" -Path $filepath
+$temp[1..$temp.Length] | Sort-Object | Add-Content -Encoding "utf8" -Path $filepath
