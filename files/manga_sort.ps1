3f067ad5132492c8f2ca5080211af2fb143f4d3d
Your Name <youremail@yourdomain.com>
2022-06-06 03:36:06 +1000

    更新

--- files/manga_sort.ps1
+++ files/manga_sort.ps1
@@ -1,5 +1,4 @@
-# $filepath = "./manga_tracking.csv"
-$filepath = "./ja_dict.csv"
+$filepath = "./manga_tracking.csv"
 $temp = Get-Content -Encoding "utf8" -Path $filepath
 $temp[0] | Set-Content -Encoding "utf8" -Path $filepath
 $temp[1..$temp.Length] | Sort-Object | Add-Content -Encoding "utf8" -Path $filepath
