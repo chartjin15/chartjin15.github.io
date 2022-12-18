d671353a4f505c2ecdd2028808140f1248776fd4
白面包 <88670752+chartjin15@users.noreply.github.com>
2022-12-18 19:52:13 +1100

    更新

--- help.sh
+++ help.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-if [ $1 = "up" ];
-then
     cd ~
     cd storage
     cd shared
     cd obsidian
     git pull origin master
+if [ $1 = "up" ];
+then
     git add -A
     git commit -m "更新"
     git push origin master
-else
-    git pull origin master
 fi
