fc41f6e0ad1b1e46937ed858d0af0207ff53d5a2
白面包 <88670752+chartjin15@users.noreply.github.com>
2024-03-28 19:19:36 +1100

    更新文件目录

--- files/manga_sort.ps1
+++
@@ -1,4 +0,0 @@
-$filepath = "./manga_tracking.csv"
-$temp = Get-Content -Encoding "utf8" -Path $filepath
-$temp[0] | Set-Content -Encoding "utf8" -Path $filepath
-$temp[1..$temp.Length] | Sort-Object | Add-Content -Encoding "utf8" -Path $filepath
--- files/py.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-m_t = pd.read_csv("files\manga_tracking.csv")
-m_t_e = m_t[m_t["已阅读话数"] > 10]
-
-print((m_t_e["出版社"].value_counts(sort=True)/m_t["出版社"].value_counts(sort=True)).sort_values(ascending=False))
