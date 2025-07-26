d8bb49ab3589e17980142418e8d72cec0fb8b660
白面包 <88670752+chartjin15@users.noreply.github.com>
2025-07-27 03:01:15 +1000

    Update writeDB.py

--- writeDB.py
+++ writeDB.py
@@ -1,30 +1,18 @@
-import datetime, readline, sqlite3, sys
+import datetime, os, readline, sqlite3, sys
 
-connect = sqlite3.connect(("对象文本" if len(sys.argv) < 2 else sys.argv[1]) + ".db")
+connect = sqlite3.connect(('text_archive' if len(sys.argv) < 2 else sys.argv[1]) + '.db')
 cursor = connect.cursor()
 
-cursor.execute(
-    """CREATE TABLE IF NOT EXISTS text (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        content TEXT NOT NULL,
-        record_time TEXT NOT NULL)
-    """
-)
+cursor.execute('CREATE TABLE IF NOT EXISTS text (id INTEGER PRIMARY KEY AUTOINCREMENT, content TEXT NOT NULL, record_time TEXT NOT NULL)')
 
-line_index = 0
+cursor.execute('SELECT COUNT(*) FROM text')
+line_index = cursor.fetchone()[0]
 while True:
     line_index += 1
-    input_string = input("{:04}| ".format(line_index))
-    if input_string in ["q", "quit"]:
+    input_string = input('{:04}| '.format(line_index))
+    if input_string in ['q', 'quit']:
         connect.close()
         break
-    cursor.execute(
-        "INSERT INTO text (content, record_time) VALUES (?, ?)",
-        (
-            input_string,
-            datetime.datetime.now(
-                datetime.timezone(datetime.timedelta(hours=8))
-            ).strftime("%Y/%m/%d %H:%M:%S"),
-        ),
-    )
+    cursor.execute('INSERT INTO text (content, record_time) VALUES (?, ?)', (input_string, datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=8))).strftime('%Y/%m/%d %H:%M:%S'),))
     connect.commit()
+    os.system(command='cls' if os.name == 'nt' else 'clear')
